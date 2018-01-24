8456fe35f3148caec98c712abe5c3673dd3dce96
AliOgretmen <aliogretmen.ch@gmail.com>
2018-01-24 11:15:36 +0100

    Single Page Applications Part 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,6 +20,8 @@ import { HomeComponent } from './home/home.component';
 import { ContactComponent } from './contact/contact.component';
 
 import { DishService } from './services/dish.service';
+import { PromotionService } from './services/promotion.service';
+
 
 import { AppRoutingModule } from './app-routing/app-routing.module';
 
@@ -46,7 +48,8 @@ import { AppRoutingModule } from './app-routing/app-routing.module';
     MatSlideToggleModule, MatToolbarModule, MatListModule, MatGridListModule,
     MatCardModule, MatIconModule, MatProgressSpinnerModule, MatDialogModule,    FlexLayoutModule,
   ],
-  providers: [DishService],
+  providers: [ DishService,
+  PromotionService ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
