0bb6527e62c19362605f52c69477ce8b08eda3ef
AliOgretmen <aliogretmen.ch@gmail.com>
2018-01-28 21:11:46 +0100

    RxJS Part 1

--- src/app/services/leader.service.ts
+++ src/app/services/leader.service.ts
@@ -2,29 +2,42 @@ import { Injectable } from '@angular/core';
 import { Leader } from '../shared/leader';
 import { LEADERS } from '../shared/leaders';
 
+import { Observable } from 'rxjs/Observable';
+
+
+import 'rxjs/add/operator/delay';
+import 'rxjs/add/observable/of';
+
+
 
 @Injectable()
 export class LeaderService {
 
   constructor() { }
 
-  getLeaders(): Promise<Leader[]> {
-    return new Promise(resolve=> {
-      setTimeout(() => resolve(LEADERS), 2000);
-    });
-
+  getLeaders(): Observable<Leader[]> {
+    return  Observable.of(LEADERS).delay(2000);
   }
 
-  getLeader(id: number): Promise<Leader> {
-    return new Promise(resolve=> {
-      setTimeout(() => resolve(LEADERS.filter((leader) => (leader.id === id))[0]), 2000);
-    });
+  getLeader(id: number): Observable<Leader> {
+    return Observable.of(LEADERS.filter((leader) => (leader.id === id))[0]).delay(2000);
   }
-  
-  getFeaturedLeader(): Promise<Leader> {
-    return new Promise(resolve => {
-      setTimeout(() => resolve(LEADERS.filter((leader) => leader.featured)[0]), 2000);
-  });
-}
+
+  getFeaturedLeader(): Observable<Leader> {
+    return Observable.of(LEADERS.filter((leader) => leader.featured)[0]).delay(2000);
+  }
+
 
 }
+/* getDishes(): Promise<Dish[]> {
+  return Observable.of(DISHES).delay(2000).toPromise();
+}
+
+getDish(id: number): Promise<Dish> {
+  return Observable.of(DISHES.filter((dish) => (dish.id === id))[0]).delay(2000).toPromise();
+}
+
+getFeaturedDish(): Promise<Dish> {
+  return Observable.of(DISHES.filter((dish) => dish.featured)[0]).delay(2000).toPromise();
+}
+} */
--- src/app/services/promotion.service.ts
+++ src/app/services/promotion.service.ts
@@ -3,50 +3,38 @@ import { Injectable } from '@angular/core';
 import { Promotion } from '../shared/promotion';
 import { PROMOTIONS } from '../shared/promotions';
 
+import { Observable } from 'rxjs/Observable';
+
+
+import 'rxjs/add/operator/delay';
+import 'rxjs/add/observable/of';
+
 @Injectable()
 export class PromotionService {
 
   constructor() { }
 
-  getPromotions(): Promise<Promotion[]> {
-    return new Promise(resolve=> {
-      // Simulate server latency with 2 second delay
-        setTimeout(() => resolve(PROMOTIONS), 2000);
-    });
+  getPromotions(): Observable<Promotion[]> {
+    return Observable.of(PROMOTIONS).delay(2000);
   }
 
-  getPromotion(id: number):Promise<Promotion> {
-    return new Promise(resolve=> {
-      // Simulate server latency with 2 second delay
-        setTimeout(() => resolve(PROMOTIONS.filter((promotion) => (promotion.id === id))[0]), 2000);
-    });
+  getPromotion(id: number): Observable<Promotion> {
+    return Observable.of(PROMOTIONS.filter((promo) => (promo.id === id))[0]).delay(2000);
   }
 
-  getFeaturedPromotion(): Promise<Promotion> {
-    return new Promise(resolve=> {
-      // Simulate server latency with 2 second delay
-        setTimeout(() => resolve(PROMOTIONS.filter((promotion) => promotion.featured)[0]), 2000);
-    });
+  getFeaturedPromotion(): Observable<Promotion> {
+    return Observable.of(PROMOTIONS.filter((promo) => promo.featured)[0]).delay(2000);
   }
 }
-/*   getDishes(): Promise<Dish[]> {
-    return  new Promise(resolve=> {
-      // Simulate server latency with 2 second delay
-        setTimeout(() => resolve(DISHES), 2000);
-    });
-  }
+/* getDishes(): Promise<Dish[]> {
+  return Observable.of(DISHES).delay(2000).toPromise();
+}
 
-  getDish(id: number): Promise<Dish> {
-    return new Promise(resolve=> {
-      // Simulate server latency with 2 second delay
-        setTimeout(() => resolve(PROMOTIONS.filter((promotion) => (promotion.id === id))[0]), 2000);
-    });
-  }
+getDish(id: number): Promise<Dish> {
+  return Observable.of(DISHES.filter((dish) => (dish.id === id))[0]).delay(2000).toPromise();
+}
 
-  getFeaturedDish(): Promise<Dish> {
-    return new Promise(resolve=> {
-      // Simulate server latency with 2 second delay
-        setTimeout(() => resolve(PROMOTIONS.filter((promotion) => promotion.featured)[0]), 2000);
-    });
-  }
+getFeaturedDish(): Promise<Dish> {
+  return Observable.of(DISHES.filter((dish) => dish.featured)[0]).delay(2000).toPromise();
+}
 } */
